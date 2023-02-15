60c25aa2599c5297a212d014cf587f91dc5bc4a9
Maks Grant <themakisun@gmail.com>
2023-02-15 23:58:22 +0200

    added script

---
+++ js/index.js
@@ -0,0 +1,25 @@
+let a = 0, b = -5, c = 10;
+
+let positive = 0, negative = 0;
+
+if (a > 0) {
+    positive++
+} else if (a < 0) {
+    negative++
+}
+
+if (b > 0) {
+    positive++
+} else if (b < 0) {
+    negative++
+}
+
+if (c > 0) {
+    positive++
+} else if (c < 0) {
+    negative++
+}
+
+0
+
+console.log(`Положительные числа: ${positive}\nОтрицательные числа: ${negative}`    )
